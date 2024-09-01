db9d6707e288ed75e7dc9cbbee818d69e92d70f0
Charles <charles42.h@gmail.com>
2024-09-01 14:28:06 +0200

    Modify header for logout & bank & add user controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('IS_AUTHENTICATED')]
+#[Route('/{_locale}/user')]
+class UserController extends AbstractController
+{
+
+    #[Route('', name: 'app_user_dashboard')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}

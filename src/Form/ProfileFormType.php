fe2c1e31d7cd9208bc5003f511cabdfee7cec510
Charles <charles42.h@gmail.com>
2024-09-06 15:59:47 +0200

    Add "Language" option in profile form

--- src/Form/ProfileFormType.php
+++ src/Form/ProfileFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,6 +20,15 @@ class ProfileFormType extends AbstractType
                 ])
             ->add('username', TextType::class, [
                 'error_bubbling' => true,
+            ])
+            ->add('preferedLocale', ChoiceType::class, [
+                'error_bubbling' => true,
+                'choices' => [
+                    'FR' => 'fr',
+                    'EN' => 'en'
+                ],
+                'expanded' => false,
+                'multiple' => false,
             ])
         ;
     }

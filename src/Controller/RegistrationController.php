1ea1f582b8431835a674f501510e5b68327d687f
Charles <charles42.h@gmail.com>
2024-09-08 11:48:58 +0200

    Move registration of user logic into its own service

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,83 +4,52 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
-use App\Repository\UserRepository;
-use App\Service\LocaleService;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\RegistrationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Uid\Uuid;
 
 class RegistrationController extends AbstractController
 {
-    public function __construct(private readonly TranslatorInterface $translator)
-    {
+    public function __construct(
+        private readonly TranslatorInterface $translator,
+        private readonly RegistrationService $registrationService,
+    ) {
     }
 
     #[Route('/{_locale}/register', name: 'app_register')]
     public function register(
         Request $request,
-        UserPasswordHasherInterface $userPasswordHasher,
-        UserRepository $userRepository,
-        EntityManagerInterface $entityManager,
-        LocaleService $localeService,
         ): Response
     {
         $user = $this->getUser();
         if($user) {
             return $this->redirectToRoute('app_user_dashboard');
         } else {
-            $user = new User;
+            $user = new User();
         }
 
-        $passwordMinLength = 8;
-        $passwordMaxLength = 32;
-
         $form = $this->createForm(RegistrationFormType::class, $user, [
-            'password_min_length' => $passwordMinLength,
-            'password_max_length' => $passwordMaxLength,
+            'password_min_length' => RegistrationService::PASSWORD_MIN_LENGTH,
+            'password_max_length' => RegistrationService::PASSWORD_MAX_LENGTH,
         ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-            $user->setSponsorCode(Uuid::v4());
-
-            if(!in_array('ROLE_ADMIN', $user->getRoles())){
-                $user->setBank(1000.0);
-            };
-            $user->setPreferedLocale($localeService->getPreferredLocale($request));
-
-            $sponsorCode = $form->get('sponsorCode')->getData();
-            if ($sponsorCode) {
-                $sponsor = $userRepository->findBySponsorCode($sponsorCode);
-                if ($sponsor) {
-                    $user->setSponsor($sponsor);
-                }
-            }
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            // do anything else you need here, like send an email
-
+            // As $form->getData() returns already a user object, we only recuperate what we need and send it to the service
+            $formData = $form->all();
+            $formData['plainPassword'] = $form->get('plainPassword')->getData();
+            $formData['sponsorCode']= $form->get('sponsorCode')->getData();
+            $this->registrationService->registerUser($user, $formData, $request);
             return $this->redirectToRoute('app_main');
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
-            'password_min_length' => $passwordMinLength,
-            'password_max_length' => $passwordMaxLength,
+            'password_min_length' => RegistrationService::PASSWORD_MIN_LENGTH,
+            'password_max_length' => RegistrationService::PASSWORD_MAX_LENGTH,
         ]);
     }
 }
